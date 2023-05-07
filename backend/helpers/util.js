1ce5cd8f3de3c5b2bf9e341fabaea15f6879e029
AiceKerime <rafiizzaturohman06@gmail.com>
2023-05-07 15:19:52 +0700

    ChatApp

--- backend/helpers/util.js
+++ backend/helpers/util.js
@@ -14,7 +14,14 @@ const encodeToken = (data) => jwt.sign(data, tokenKey, { expiresIn: '1h' })
 
 const decodeToken = (data) => jwt.verify(data, tokenKey)
 
-
+const isLoggedIn = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(new Response(error, false))
+    }
+}
 
 module.exports = {
     Response,
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,36 @@
 var express = require('express');
+const User = require('../models/user');
+const { Response } = require('../helpers/util');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async (req, res, next) => {
+  try {
+    const getUser = await User.find().populate('chats')
+    res.status(200).json(new Response(getUser))
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(new Response(error, false))
+  }
 });
+
+router.post('/', async (req, res, next) => {
+  try {
+    const createUser = await User.create(req.body)
+    res.status(201).json(new Response(createUser))
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(new Response(error, false))
+  }
+});
+
+router.delete('/:_id', async (req, res, next) => {
+  try {
+    const deleteUser = await User.findOneAndDelete({_id: req.params._id})
+    res.status(200).json(new Response())
+  } catch (error) {
+    
+  }
+})
 
 module.exports = router;
